f684f6331fbf590669909becaff2dc4bafe98546
TobyMike-max <osinubi.michael@gmail.com>
2022-08-03 18:02:30 +0100

    [Modified]: Added more test cases for BaseModel Instance

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,9 +3,9 @@
 """Defines unittests for base_model.py.
 
 Unittest classes:
-    TestBaseModel_instantiation - line 16
-    TestBase_to_json_string - line 98
-    TestBase_save_to_file - line 156
+    TestBaseModel_Instantiation - line 15
+    TestBase_Instance_Print - line 51
+    TestBase_Save_Method - line 59
     TestBase_from_json_string - line 234
 """
 from datetime import datetime
@@ -47,6 +47,38 @@ class TestBaseModel_Instantiation(unittest.TestCase):
     def test_Created_atInstance(self):
         b1 = BaseModel()
         self.assertIsInstance(b1.updated_at, datetime)
+
+class TestBaseModel_Instance_Print(unittest.TestCase):
+    """Unittest for testing the return value of __str__ method."""
+
+    def test_str_return(self):
+        b1 = BaseModel()
+        ret = "[{}] ({}) {}".format("BaseModel", b1.id, str(b1.__dict__))
+        self.assertEqual(print(b1), print(ret))
+
+class TestBaseModel_Save_Method(unittest.TestCase):
+    """Unittest for testing the save method."""
+
+    def test_validates_save(self):
+        b1 = BaseModel()
+        updated_at_1 = b1.updated_at
+        b1.save()
+        updated_at_2 = b1.updated_at
+        self.assertNotEqual(updated_at_1, updated_at_2)
+
+class TestBaseModel_to_Dict_Method(unittest.TestCase):
+    """Unittest for testing the to_dict method."""
+
+    def test_validates_to_dict(self):
+        b1 = BaseModel()
+        dic = b1.to_dict()
+        self.assertEqual(dic, b1.__dict__)
+
+    def test_className_present(self):
+        b1 = BaseModel()
+        dic = b1.to_dict()
+        className = {"__class__": "BaseModel"}
+        assertEqual(dic, dic | className)
 
 if __name__ == "__main__":
     unittest.main()
