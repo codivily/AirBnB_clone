ec4d0a1e3cfb115435f666e7151faa3b0c67ab1e
TobyMike-max <osinubi.michael@gmail.com>
2022-08-03 17:05:34 +0100

    [Modified]: Added new test cases for instance attributes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,14 +14,38 @@ from models.base_model import BaseModel
 class TestBaseModel_Instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
 
-    def test_no_arg(self):
+    def test_IsInstanceOf(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1, BaseModel)
+
+    def test_ContainsId(self):
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "id"))
+
+    def test_IdType(self):
         b1 = BaseModel()
         self.assertEqual(type(b1.id), str)
 
-    def test_two_basemodels(self):
+    def test_CompareTwoInstancesId(self):
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.id, b2.id)
+
+    def test_ContainsCreated_at(self):
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "created_at"))
+
+    def test_Created_atInstance(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1.created_at, datetime)
+
+    def test_ContainsUpdated_at(self):
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "updated_at"))
+
+    def test_Created_atInstance(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1.updated_at, datetime)
 
 if __name__ == "__main__":
     unittest.main()
