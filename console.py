e44959d23b5f652724f22cea1b263a4f0a1f3535
root <root@u-station>
2022-08-05 20:53:38 +0000

    Added: all and update command

--- console.py
+++ console.py
@@ -55,9 +55,9 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
 
-        constructor = globals().get(strs[0], None)
-        if constructor is None:
-            print("** class doesn't exists **") 
+        kclass = globals().get(strs[0], None)
+        if kclass is None:
+            print("** class doesn't exist **") 
             return
 
         if len(strs) != 2:
@@ -68,6 +68,68 @@ class HBNBCommand(cmd.Cmd):
            print(storage.all()[strs[1]])
         else:
             print("** no instance found **")
+    
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        kclass = globals().get(arg, None)
+        if kclass is None:
+            print("** class doesn't exist **")
+            return
+        for k, v in storage.all().items():
+            if v.__class__.__name__ != arg:
+                continue
+            print(v)
+
+    def do_update(self, arg):
+        """Update an instance base of class name and id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        strs = arg.split(" ")
+        count = len(strs)
+        if not strs:
+            print("** class name missing **")
+            return
+        
+        kclass = globals().get(strs[0], None)
+        if kclass is None:
+            print ("** class doesn't exist **")
+            return
+        className = strs[0]
+
+        if count < 2:
+            print("** instance id missing **")
+            return
+        objId = strs[1]
+
+        if  objId not in storage.all():
+            print("** no instance found **")
+            return
+
+        obj = storage.all()[objId]
+
+        if count < 3:
+            print("** attribute name missing **")
+            return
+        attr_name = strs[2]
+
+        if count < 4:
+            print("** value missing **")
+            return
+        attr_value = strs[3]
+
+        if hasattr(obj, attr_name):
+            setattr(obj, attr_name, type(getattr(obj, attr_name))(attr_value))
+        else:
+            setattr(obj, attr_name, attr_value)
+
+        obj.save()
+
+        
+
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
