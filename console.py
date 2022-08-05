501bf8c6cd2a9c4f43daaf26e0586ce9c59c39cb
TobyMike-max <osinubi.michael@gmail.com>
2022-08-05 20:20:09 +0100

    [Modified]: Code refactoring

--- console.py
+++ console.py
@@ -8,16 +8,18 @@ class HBNBCommand(cmd.Cmd):
     """Class containing the entry point of the command interperter"""
     prompt = "(hbnb) "
 
+    doc_header = 'Documented commands (type help <topic>):'
+
     def do_quit(self, arg):
+        """Command to exit the program."""
         exit()
 
-    def do_help(self, arg):
-        if arg == 'quit':
-            print('Quit command to exit the program')
-        else:
-            print("Documented command (type help <topic>):")
-            print("=======================================")
-            print("EOF help quit")
+    def help_quit(self):
+        print('Quit command to exit the program')
+
+    def do_EOF(self, arg):
+        """Command to exit the program."""
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
