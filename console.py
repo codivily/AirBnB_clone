29f9ee41e9642e6203242de23800f4d094a06e5b
TobyMike-max <osinubi.michael@gmail.com>
2022-08-05 20:28:51 +0100

    [Modified]: Handle emptyline method

--- console.py
+++ console.py
@@ -8,18 +8,21 @@ class HBNBCommand(cmd.Cmd):
     """Class containing the entry point of the command interperter"""
     prompt = "(hbnb) "
 
-    doc_header = 'Documented commands (type help <topic>):'
+    #doc_header = 'Documented commands (type help <topic>):'
 
     def do_quit(self, arg):
         """Command to exit the program."""
         exit()
 
-    def help_quit(self):
-        print('Quit command to exit the program')
-
     def do_EOF(self, arg):
         """Command to exit the program."""
         return True
+
+    def help_quit(self):
+        print('Quit command to exit the program\n')
+
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
