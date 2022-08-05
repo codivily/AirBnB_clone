157caab80d36dea6317e48eecaa940c4e00697db
root <root@u-station>
2022-08-05 21:33:38 +0000

    Added: State, City, Amenity, Place, Review models

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""`Amenity` class module"""
+
+from .base_model import BaseModel
+
+class Amenity(BaseModel):
+    """A class that represent an amenity"""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""`Review` class module"""
+
+from .base_model import BaseModel
+
+class Review(BaseModel):
+    """A class that represent a review"""
+    place_id = ""
+    user_id = ""
+    text = ""
