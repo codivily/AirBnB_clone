74ad98a4eb76ceb7a08fbbab6d4a15e988aeb485
TobyMike-max <osinubi.michael@gmail.com>
2022-08-05 17:21:31 +0100

    [Create]: File containing the command interpreter class

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class containing the entry point of the command interpreter."""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command Interpreter class."""
+    def do_help:
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
